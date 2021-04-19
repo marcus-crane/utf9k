d8a6d1c700f27bba7fcce87c560fe6e6cca51b83
Marcus Crane <marcus@utf9k.net>
2021-04-19 19:06:38 +1200

    Set up currently watching script

--- netlify/functions/currently_watching.js
+++ netlify/functions/currently_watching.js
@@ -1,8 +1,30 @@
+const API_ENDPOINT = "https://api.trakt.tv/users/sentry/watching"
+
 exports.handler = async function(event, context) {
+  const clientID = process.env.CLIENT_ID
+
+  headers = {
+    "Content-Type": "application/json",
+    "trakt-api-version": 2,
+    "trakt-api-key": clientID
+  }
+
+  let response
+  try {
+    response = await fetch(API_ENDPOINT, headers=headers)
+  } catch (err) {
+    return {
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
+    }
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({
-      "hi": 123
+      data: response
     })
   }
 }
