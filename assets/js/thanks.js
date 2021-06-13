d33cbe01230d82881b9438cd6d8320fa9fed4196
Marcus Crane <marcus@utf9k.net>
2021-06-13 23:27:56 +1200

    Update thanks.js

--- assets/js/thanks.js
+++ assets/js/thanks.js
@@ -3,8 +3,7 @@ const thanksURL = "https://gunslinger.utf9k.net/api/v1/thanks"
 function sayThanksAnonymously() {
   const currentPage = window.location.href || document.location.href
   const headers = new Headers({
-    "Content-Type": "application/json",
-    "User-Agent": "WeDontNeedToKnow" // This unfortunately has no effect as far as I can tell :(
+    "Content-Type": "application/json"
   })
   const credentials = "omit" // This site has no cookies but lets omit them anyway, just in case!
   const body = JSON.stringify({ "liked_url": currentPage })
