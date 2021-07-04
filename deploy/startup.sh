0f807c3e425b293dfccd6bdb8257e57c28e3ba22
Marcus Crane <marcus@utf9k.net>
2021-07-04 18:28:39 +1200

    Fix prom exporter binary name

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -2,4 +2,4 @@
 echo "Booting up nginx"
 nginx -g "daemon on;"
 echo "Booting up Prometheus nginx exporter"
-./usr/bin/prometheus-nginxlog-exporter -config-file /tmp/config.hcl
+./usr/bin/nginx-prometheus-exporter -config-file /tmp/config.hcl
