052c68047ae3923994475cf25bd0db2b93b3e6d4
Marcus Crane <marcus@utf9k.net>
2021-07-04 18:37:53 +1200

    chmod exporter

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -2,4 +2,5 @@
 echo "Booting up nginx"
 nginx -g "daemon on;"
 echo "Booting up Prometheus nginx exporter"
-./usr/bin/nginx-prometheus-exporter -config-file /tmp/config.hcl
+chmod +x /usr/bin/nginx/prometheus-exporter
+/usr/bin/nginx-prometheus-exporter -config-file /tmp/config.hcl
