c296475dee6f78d563bdaad065b9112d03ef50cf
Marcus Crane <marcus@utf9k.net>
2021-08-12 21:29:25 +1200

    Disable log exporter and metrics exporter

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -4,8 +4,3 @@ rm /var/log/nginx/access.log
 rm /var/log/nginx/error.log
 echo "Booting up nginx"
 nginx -g "daemon on;"
-sleep 5
-echo "Booting up Vector"
-vector -c /etc/vector/vector.toml
-echo "Booting up Prometheus nginxlog exporter"
-/usr/bin/prometheus-nginxlog-exporter -config-file /tmp/config.hcl
