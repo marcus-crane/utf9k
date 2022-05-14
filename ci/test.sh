8d2902284b60fe1ea4e5664d45a3f7fc3bc1b14e
Marcus Crane <marcus@utf9k.net>
2022-05-14 15:00:59 +1200

    Add cue validation for games list

--- ci/test.sh
+++ ci/test.sh
@@ -8,3 +8,6 @@ echo "~ All book entries are semantically valid"
 
 cue vet data/gags.yml validation/gags.cue
 echo "~ All gag lines are semantically valid"
+
+cue vet data/games.yml validation/games.cue
+echo "~ All game entries are semantically valid"
