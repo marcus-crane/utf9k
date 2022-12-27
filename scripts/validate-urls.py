6d111a0552368181c8f01eb483493080fd0b7714
Marcus Crane <marcus@utf9k.net>
2022-12-27 22:08:49 +1300

    Quick hack to save diffable version

--- scripts/validate-urls.py
+++ scripts/validate-urls.py
@@ -7,11 +7,13 @@ import requests
 from common import CUR_DIR
 
 DATA_FILE = "data/links.json"
+DATA_FILE_NO_HEADERS = "data/links-noheaders.json"
 STATIC_SITE = f"{CUR_DIR}/../content"
 URL_RE = r"((?<=[^a-zA-Z0-9])(?:https?\:\/\/|[a-zA-Z0-9]{1,}\.{1}|\b)(?:\w{1,}\.{1}){1,5}(?:com|org|edu|gov|uk|net|ca|de|jp|fr|au|us|ru|ch|it|nl|se|no|es|mil|iq|io|ac|ly|sm){1}(?:\/[a-zA-Z0-9-_.]{1,})*)"
 include = set(['assets', 'content', 'layouts'])
 
 LINKS = {}
+LINKS_NO_HEADERS = {}
 
 for folder in include:
     for root, dirs, files in os.walk(folder, topdown=True):
@@ -36,19 +38,32 @@ for folder in include:
                     'source': full_path,
                     'url': url
                 }
+                results_no_headers = {
+                    'source': full_path,
+                    'url': url
+                }
                 try:
                     r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) WebKit/8610 (KHTML, like Gecko) Mobile/18C66 [FBAN/FBIOS;FBDV/iPhone13,4;FBMD/iPhone;FBSN/iOS;FBSV/14.3;FBSS/3;FBID/phone;FBLC/en_US;FBOP/5]'})
                     results['alive'] = r.ok
+                    results_no_headers['alive'] = r.ok
                     results['status'] = r.status_code
+                    results_no_headers['status'] = r.status_code
                     results['headers'] = dict(r.headers)
                 except requests.exceptions.ConnectionError as Exception:
                     results['alive'] = False
+                    results_no_headers['alive'] = False
                     results['status'] = 000
+                    results_no_headers['alive'] = 000
                     results['headers'] = {}
                 LINKS[url] = results
+                LINKS_NO_HEADERS[url] = results_no_headers
+                
                 print(f"{url} at {full_path}")
 
 with open(DATA_FILE, 'w') as file:
     json.dump(LINKS, file, indent=2, sort_keys=True)
+
+with open(DATA_FILE_NO_HEADERS, 'w') as file:
+    json.dump(LINKS_NO_HEADERS, file, indent=2, sort_keys=True)
 
 print("~ URLs validated")
