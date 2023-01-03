fe3fce8faff455dab24c6b353d62df4af4e32afe
Marcus Crane <marcus@utf9k.net>
2023-01-03 19:11:06 +1300

    Surface captions and support clicking image to close

--- assets/js/image-viewer.js
+++ assets/js/image-viewer.js
@@ -1,12 +1,25 @@
 // Largely adapted from image viewer code bundled with the Nintendo Switch
-document.querySelectorAll("#content img").forEach(function(el) {
+document.querySelectorAll("article img").forEach(function(el) {
   el.addEventListener("click", function() {
     const imageViewer = document.createElement("div")
     const fullsizeImage = document.createElement("img")
+    const imageCaption = document.createElement("p")
+    // Used to surface links that would otherwise redirect the user on click to somewhere besides image src
+    const imageLink = el.getAttribute("altsrc")
+    const imageLinkTitle = el.getAttribute("alttitle")
     
     fullsizeImage.src = el.src
 
+    imageCaption.innerHTML = el.getAttribute("alt")
+
+    if (imageLink !== null) {
+      let imageTitle = imageLinkTitle === null ? imageLink : imageLinkTitle
+      imageCaption.innerHTML += "<br /><br />"
+      imageCaption.innerHTML += `You can learn more by visiting <a target="_blank" href="${imageLink}">${imageTitle}</a>`
+    }
+
     imageViewer.appendChild(fullsizeImage)
+    imageViewer.appendChild(imageCaption)
 
     let imgViewContainer = document.getElementById("fullscreen")
     let body = document.querySelector("body")
@@ -14,6 +27,12 @@ document.querySelectorAll("#content img").forEach(function(el) {
     imageViewer.className = "fullscreen"
     imgViewContainer.appendChild(imageViewer)
     imgViewContainer.style.display = "block"
+    
+    // Footnotes using littlefoot come with a z-index that interferes with the overlay
+    // so we temporarily sink them
+    document.querySelectorAll("button.littlefoot-footnote__button").forEach(function(fn) {
+      fn.style.zIndex = 0
+    })
 
     let closeViewer = function(e) {
       if ("hide" === e.animationName) {
@@ -34,6 +53,9 @@ document.querySelectorAll("#content img").forEach(function(el) {
     function onCloseEvent() {
       imgViewContainer.classList.add("hide");
       imgViewContainer.addEventListener("animationend", closeViewer);
+      document.querySelectorAll("button.littlefoot-footnote__button").forEach(function(fn) {
+        fn.style.removeProperty("z-index")
+      })
     }
 
     // Keyboard controls
@@ -56,6 +78,7 @@ document.querySelectorAll("#content img").forEach(function(el) {
     }
 
     closeButton.onclick = onCloseEvent
+    fullsizeImage.onclick = onCloseEvent
     body.addEventListener("keydown", listenForKeyEvent)
     imageViewer.appendChild(closeButton)
   })
