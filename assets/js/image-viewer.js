b6a92f4cb6f08ca12131edf2231b2f31ebfa8159
Marcus Crane <marcus@utf9k.net>
2023-05-07 13:59:38 +1200

    Add hover overlay to hint at image expansion ability
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- assets/js/image-viewer.js
+++ assets/js/image-viewer.js
@@ -1,6 +1,29 @@
 // Largely adapted from image viewer code bundled with the Nintendo Switch
 document.querySelectorAll("article img").forEach(function(el) {
-  el.addEventListener("click", function() {
+  const imageLoaded = el.complete && el.naturalHeight !== 0
+  console.log(imageLoaded)
+
+  if (imageLoaded) {
+    addOverlay(el)
+  } else {
+    el.addEventListener("load", el => addOverlay(el.target))
+  }
+})
+
+function addOverlay(el) {
+  const overlayDiv = document.createElement("div")
+  overlayDiv.classList.add("overlay")
+  overlayDiv.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+      <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+    </svg>
+  `
+  overlayDiv.style.height = `${el.height}px`
+  overlayDiv.style.width = `${el.width}px`
+  el.insertAdjacentElement("beforebegin", overlayDiv)
+
+  overlayDiv.addEventListener("click", function() {
     const imageViewer = document.createElement("div")
     const fullsizeImage = document.createElement("img")
     const imageCaption = document.createElement("p")
@@ -82,4 +105,4 @@ document.querySelectorAll("article img").forEach(function(el) {
     body.addEventListener("keydown", listenForKeyEvent)
     imageViewer.appendChild(closeButton)
   })
-})
+}
