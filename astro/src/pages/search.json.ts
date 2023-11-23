5ae203a5cf1a0c959396727cd47d3912d11f222e
Marcus Crane <marcus@utf9k.net>
2023-11-23 19:57:52 +1300

    Wire in title and description fields
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- astro/src/pages/search.json.ts
+++ astro/src/pages/search.json.ts
@@ -3,15 +3,15 @@ import { getCollection } from "astro:content";
 const questions = await getCollection("questions");
 
 export async function GET() {
-    const index = questions.map(q => {
-        return ({
-            content: q.body,
-            slug: q.slug,
-            tags: q.data.tags,
-            title: q.data.title
-        })
+  const index = questions.map(q => {
+    return ({
+      content: q.body,
+      slug: q.slug,
+      tags: q.data.tags,
+      title: q.data.title
     })
-    return new Response(
-        JSON.stringify(index)
-    )
+  })
+  return new Response(
+    JSON.stringify(index)
+  )
 }
