7fc7ee6b1d8152275bd9d29769cbf98847def5fb
Marcus Crane <marcus@utf9k.net>
2023-11-28 19:44:07 +1300

    Fork remark and pass in remarkRehype options
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- lume/_config.ts
+++ lume/_config.ts
@@ -4,13 +4,13 @@ import esbuild from "lume/plugins/esbuild.ts";
 import jsx_preact from "lume/plugins/jsx_preact.ts";
 import feed from "lume/plugins/feed.ts";
 import reading_info from "lume/plugins/reading_info.ts";
-import remark from "lume/plugins/remark.ts";
 import vento from "lume/plugins/vento.ts";
 
 import rehypePrettyCode from "npm:rehype-pretty-code";
 
 import prettier from "npm:prettier"
 
+import remark from "./remark.ts";
 import { fnv_1a } from "./utils.ts"
 
 const site = lume({
@@ -42,7 +42,12 @@ site.use(feed({
 site.use(jsx_preact());
 site.use(reading_info());
 site.use(remark({
-    rehypePlugins: [rehypePrettyCode]
+    rehypePlugins: [rehypePrettyCode],
+    remarkRehype: {
+        footnoteLabelProperties: {
+            className: ['sr-only', 'littlefoot--print']
+        }
+    }
 }));
 site.use(vento());
 site.use(esbuild());
--- lume/js/footnotes.js
+++
@@ -1,3 +0,0 @@
-import littlefoot from "npm:littlefoot"
-
-littlefoot()
