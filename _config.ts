fc04ab32870c9ff596b197d70f4ef68d28fddb16
Marcus Crane <marcus@utf9k.net>
2023-12-03 23:40:04 +1300

    Pull some packages out into deno.json
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- _config.ts
+++ _config.ts
@@ -9,8 +9,8 @@ import vento from "lume/plugins/vento.ts";
 import nunjucks from "lume/plugins/nunjucks.ts"
 
 // NPM
-import rehypePrettyCode from "npm:rehype-pretty-code@0.11.0";
-import prettier from "npm:prettier@3.0.0"
+import rehypePrettyCode from "rehype-pretty-code";
+import prettier from "prettier"
 
 // Deno / ESM
 import { fromHtmlIsomorphic } from 'https://esm.sh/hast-util-from-html-isomorphic@2.0.0'
