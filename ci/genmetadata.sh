304aa6038dc7974098e06c9ff75ea87fd5930f34
Marcus Crane <marcus@utf9k.net>
2023-12-03 18:54:20 +1300

    Remove fancylink test
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- ci/genmetadata.sh
+++ ci/genmetadata.sh
@@ -9,7 +9,6 @@ echo "Generating site metadata"
 
 python "$SCRIPTS/gather-filesizes.py"
 # python "$SCRIPTS/gather-image-metadata.py" TODO: Fix
-python "$SCRIPTS/generate-fancylinks.py"
 python "$SCRIPTS/sync-manga.py"
 python "$SCRIPTS/sync-games.py"
 python "$SCRIPTS/sync-reader.py"
