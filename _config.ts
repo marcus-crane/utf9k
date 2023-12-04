c1efbe5ac461d4eef784f5ec5240f1325ce15c27
Marcus Crane <marcus@utf9k.net>
2023-12-05 12:23:39 +1300

    Add meta tags
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- _config.ts
+++ _config.ts
@@ -2,6 +2,7 @@
 import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
 import esbuild from "lume/plugins/esbuild.ts";
+import metas from "lume/plugins/metas.ts";
 import jsx_preact from "lume/plugins/jsx_preact.ts";
 import feed from "lume/plugins/feed.ts";
 import reading_info from "lume/plugins/reading_info.ts";
@@ -59,7 +60,7 @@ site.use(feed({
     query: "category=blog",
     info: {
         title: "utf9k",
-        description: "You are online and logged on to my personal ",
+        description: "You are online and logged on to my personal site",
         lang: "en",
         generator: true
     },
@@ -104,6 +105,7 @@ site.use(nunjucks({
         }
     }
 }))
+site.use(metas())
 
 // TODO: ESBuild + content hashing
 site.copy("css");
