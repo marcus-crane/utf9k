399a2f724e1a576357afbb9109aa8d92908d3679
Marcus Crane <marcus@utf9k.net>
2023-12-17 17:45:43 +1300

    Extend RSS feed a bit
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- src/utils/rfc822.ts
+++ src/utils/rfc822.ts
@@ -1,8 +1,15 @@
 import dayjs from "dayjs"
+import utc from "dayjs/plugin/utc"
+import timezone from "dayjs/plugin/timezone"
+import advancedFormat from "dayjs/plugin/advancedFormat"
 
-const RFC822_DATE_FORMAT = 'ddd, DD MMM YYYY HH:mm:ss ZZ'
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(advancedFormat)
+
+const RFC822_DATE_FORMAT = 'ddd, DD MMM YYYY HH:mm:ss [GMT]'
 
 // Format date as RFC822 format, primarily for RSS feeds
 export const dateToRfc822 = function(date: Date): string {
-    return dayjs(date).format(RFC822_DATE_FORMAT)
+    return dayjs(date).tz("Etc/GMT").format(RFC822_DATE_FORMAT)
 }
