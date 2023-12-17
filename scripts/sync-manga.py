4e77ea7cd2417cf14362ce0b5daf5dd1ba89300f
Marcus Crane <marcus@utf9k.net>
2023-12-17 22:55:03 +1300

    Get manga cover dimensions
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- scripts/sync-manga.py
+++ scripts/sync-manga.py
@@ -2,6 +2,7 @@ import json
 import os
 
 import requests
+from PIL import ImageFile
 
 from common import CUR_DIR
 
@@ -46,15 +47,24 @@ r = requests.post("https://graphql.anilist.co", json={'query': query})
 results = r.json()['data']['MediaListCollection']['lists'][0]['entries']
 
 for result in results:
+    coverUrl = result['media']['coverImage']['extraLarge']
+    headers = {'Range': 'bytes=0-2000000'}
+    r = requests.get(coverUrl, stream=True, headers=headers)
+
+    p = ImageFile.Parser()
+    p.feed(r.content)
+    width, height = p.image.size
     manga.append({
-        'progress': result['progress'],
-        'title': result['media']['title']['userPreferred'],
-        'chapters': result['media']['chapters'],
-        'cover': result['media']['coverImage']['extraLarge'],
-        'color': result['media']['coverImage']['color'],
-        'status': result['media']['status'],
-        'url': result['media']['siteUrl'],
-        'author': result['media']['staff']['nodes'][0]['name']['full']
+      'progress': result['progress'],
+      'title': result['media']['title']['userPreferred'],
+      'chapters': result['media']['chapters'],
+      'cover': coverUrl,
+      'cover_height': height,
+      'cover_width': width,
+      'color': result['media']['coverImage']['color'],
+      'status': result['media']['status'],
+      'url': result['media']['siteUrl'],
+      'author': result['media']['staff']['nodes'][0]['name']['full']
     })
 
 manga = sorted(manga, key=lambda x: x['title'])
