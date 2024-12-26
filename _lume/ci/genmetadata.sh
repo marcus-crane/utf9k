28d10e11914a864dc8afd5c21a4d4545c03ce6f5
Marcus Crane <marcus@utf9k.net>
2024-12-26 19:02:00 +1300

    Stub out scripts
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- _lume/ci/genmetadata.sh
+++ _lume/ci/genmetadata.sh
@@ -7,8 +7,8 @@ SCRIPTS="$TOPLEVEL/scripts"
 
 echo "Generating site metadata"
 
-python "$SCRIPTS/gather-filesizes.py"
+# python "$SCRIPTS/gather-filesizes.py"
 # python "$SCRIPTS/gather-image-metadata.py" TODO: Fix
 python "$SCRIPTS/sync-manga.py"
 python "$SCRIPTS/sync-games.py"
-python "$SCRIPTS/sync-reader.py"
+# python "$SCRIPTS/sync-reader.py"
