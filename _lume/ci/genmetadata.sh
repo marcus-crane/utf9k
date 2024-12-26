e6d38a5073c9c9186fedb1886125e50fe18f78aa
Marcus Crane <marcus@utf9k.net>
2024-12-26 19:02:09 +1300

    Add comment reminder about metadata script
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- _lume/ci/genmetadata.sh
+++ _lume/ci/genmetadata.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+# Sometimes I create subfolders with SSGs I'm trying out but I don't want to
+# use stale data so I just point this at the top level scripts instead
 TOPLEVEL=$(git rev-parse --show-toplevel)
 SCRIPTS="$TOPLEVEL/scripts"
 
