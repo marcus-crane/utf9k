56ef7975dacee5bf87e9e840433244aaad54c2cc
Marcus Crane <marcus@utf9k.net>
2025-02-06 21:26:41 +1300

    Add re-read of Half-Life 2
    Signed-off-by: Marcus Crane <marcus@utf9k.net>

--- scripts/generate-hardcover-csv.py
+++ scripts/generate-hardcover-csv.py
@@ -31,35 +31,42 @@ with open(DATA_FILE, "r") as file:
         writer = csv.DictWriter(csvfile, fieldnames=field_names)
         writer.writeheader()
 
-        for book in books[7]['books']:
-            title = book['title']
-            subtitle = book['subtitle']
-            if book['subtitle'] is not None:
-                title = f"{title}: {subtitle}"
-            author = book['author']
-            isbn13 = book['isbn13']
-            asin = book['asin']
-            date_added = book['date_added']
-            date_started = book['date_started']
-            date_finished = book['date_finished']
+        for bookYear in books:
+            for book in bookYear['books']:
+                title = book['title']
+                subtitle = book['subtitle']
+                if book['subtitle'] is not None:
+                    title = f"{title}: {subtitle}"
+                if not book.get('author', False):
+                    print(title)
+                author = book['author']
+                isbn13 = book['isbn13']
+                asin = book['asin']
+                date_added = book['date_added']
+                date_started = book['date_started']
+                date_finished = book['date_finished']
 
-            if date_added is not None:
-                date_added = date_added.strftime("%Y-%m-%d")
-            
-            if date_started is not None:
-                date_started = date_started.strftime("%Y-%m-%d")
+                if date_added is not None:
+                    date_added = date_added.strftime("%Y-%m-%d")
+                
+                if date_started is not None:
+                    date_started = date_started.strftime("%Y-%m-%d")
 
-            if date_finished is not None:
-                date_finished = date_finished.strftime("%Y-%m-%d")
+                if date_finished is not None:
+                    date_finished = date_finished.strftime("%Y-%m-%d")
+                
+                status = 'Read'
+                if book['progress'] != 100:
+                    status = 'Currently Reading'
 
-            writer.writerow({
-                'Title': title,
-                'Author': author,
-                'Status': 'Read',
-                'ISBN 13': isbn13,
-                'ASIN': asin,
-                'Date Added': date_added,
-                'Date Started': date_started,
-                'Date Finished': date_finished
-            })
+                writer.writerow({
+                    'Title': title,
+                    'Author': author,
+                    'Status': status,
+                    'ISBN 13': isbn13,
+                    'ASIN': asin,
+                    'Date Added': date_added,
+                    'Date Started': date_started,
+                    'Date Finished': date_finished
+                })
 
